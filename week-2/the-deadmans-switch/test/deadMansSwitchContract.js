e4786940d65373734135cf7affae37653b81db4f
Rohan Sawant <rohansawantct83@gmail.com>
2020-06-21 21:00:49 +0530

    Deadman switch working

--- week-2/the-deadmans-switch/test/deadMansSwitchContract.js
+++ week-2/the-deadmans-switch/test/deadMansSwitchContract.js
@@ -20,7 +20,6 @@ contract("DeadMansSwitchContract", accounts => {
         await DMSContract.still_alive({ from: accounts[0] });
 
         const res = await DMSContract.getData();
-        console.log(res)
         assert(res[0] == accounts[0])
         assert(res[1] <= accounts[1])
     })
@@ -36,7 +35,6 @@ contract("DeadMansSwitchContract", accounts => {
         await DMSContract.still_alive({ from: accounts[0] });
 
         const res = await DMSContract.getData();
-        console.log(res)
         assert(res[0] == accounts[0])
         assert(res[1] <= accounts[1])
         assert(res[2].toNumber() <= time + 1000)
@@ -52,7 +50,21 @@ contract("DeadMansSwitchContract", accounts => {
         const time = Math.round(new Date().getTime() / 1000);
         await DMSContract.still_alive({ from: accounts[0] });
 
+        //transfer balance to contract
+        var Web3 = require('web3');
+        const url = "http://127.0.0.1:7545";
+        var web3 = new Web3(new Web3.providers.HttpProvider(url));
+
+        let DMSJson = require('../build/contracts/DeadmansSwitch.json')
+
+        var abi = DMSJson["abi"];
+        var contractAddress = DMSContract.address;
+        var dmsContract = new web3.eth.Contract(abi, contractAddress);
+        let send = await web3.eth.sendTransaction({ from: accounts[0], to: contractAddress, value: web3.utils.toWei("1", "ether") });
+        console.log(send)
+
         const res = await DMSContract.getData();
+
         assert(res[0] == accounts[0])
         assert(res[1] <= accounts[1])
         assert(res[2].toNumber() <= time + 1000)
